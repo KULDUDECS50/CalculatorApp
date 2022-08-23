0da7a2f9493befa86c1a6cdc1377ee76675c4dad
Kuldeep <63112459+KULDUDECS50@users.noreply.github.com>
2022-08-23 13:02:19 -0500

    Completed BASE Requirments
    - created strings for button names
    - works properly when user uses int values
    -fonts and colors are easy on the eyes
    - completed on time
    - made custom strings and colors

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -15,50 +15,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-//    public void findSum(View view) {
-//        EditText number1ET = findViewById(R.id.num1ET);
-//        EditText number2ET = findViewById(R.id.num2ET);
-//        TextView numberSumTV = findViewById(R.id.resultTV);
-//
-//        int num1 = Integer.parseInt((number1ET.getText().toString()));
-//        int num2 = Integer.parseInt((number2ET.getText().toString()));
-//        int sum = num1 + num2;
-//
-//        numberSumTV.setText("" + sum);
-//    }
-//    public void findDiff(View view) {
-//        EditText number1ET = findViewById(R.id.num1ET);
-//        EditText number2ET = findViewById(R.id.num2ET);
-//        TextView numberSumTV = findViewById(R.id.resultTV);
-//
-//        int num1 = Integer.parseInt((number1ET.getText().toString()));
-//        int num2 = Integer.parseInt((number2ET.getText().toString()));
-//        int difference = num1 - num2;
-//
-//        numberSumTV.setText("" + difference);
-//    }
-//    public void findDiv(View view) {
-//        EditText number1ET = findViewById(R.id.num1ET);
-//        EditText number2ET = findViewById(R.id.num2ET);
-//        TextView numberSumTV = findViewById(R.id.resultTV);
-//
-//        int num1 = Integer.parseInt((number1ET.getText().toString()));
-//        int num2 = Integer.parseInt((number2ET.getText().toString()));
-//        double difference = (double)num1/num2;
-//
-//        numberSumTV.setText("" + difference);
-//    }
-//    public void findProd(View view) {
-//        EditText number1ET = findViewById(R.id.num1ET);
-//        EditText number2ET = findViewById(R.id.num2ET);
-//        TextView numberSumTV = findViewById(R.id.resultTV);
-//
-//        int num1 = Integer.parseInt((number1ET.getText().toString()));
-//        int num2 = Integer.parseInt((number2ET.getText().toString()));
-//        int product = num1 * num2;
-//
-//        numberSumTV.setText("" + product);
-//    }
+    public void findSum(View view) {
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+
+        int num1 = Integer.parseInt((number1ET.getText().toString()));
+        int num2 = Integer.parseInt((number2ET.getText().toString()));
+        int sum = num1 + num2;
+
+        numberSumTV.setText("" + sum);
+    }
+    public void findDiff(View view) {
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+
+        int num1 = Integer.parseInt((number1ET.getText().toString()));
+        int num2 = Integer.parseInt((number2ET.getText().toString()));
+        int difference = num1 - num2;
+
+        numberSumTV.setText("" + difference);
+    }
+    public void findDiv(View view) {
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+
+        int num1 = Integer.parseInt((number1ET.getText().toString()));
+        int num2 = Integer.parseInt((number2ET.getText().toString()));
+        double difference = (double)num1/num2;
+
+        numberSumTV.setText("" + difference);
+    }
+    public void findProd(View view) {
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+
+        int num1 = Integer.parseInt((number1ET.getText().toString()));
+        int num2 = Integer.parseInt((number2ET.getText().toString()));
+        int product = num1 * num2;
+
+        numberSumTV.setText("" + product);
+    }
 
 
 }
